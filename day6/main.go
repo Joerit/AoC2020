a3275fab706a882415e92679145dbd8794263d46
ratty <ratty@inuits.eu>
2020-12-15 12:24:34 +0100

    day6 pt2

--- day6/main.go
+++ day6/main.go
@@ -23,12 +23,23 @@ func readInput(filename string) string {
 
 func parse(str string) *bit.Set {
 	lines := strings.Split(str, "\n")
-	set := bit.New()
+	set := bit.New().AddRange('a', 'z'+1)
 	for _, line := range lines {
-		for _, char := range line {
-			set.Add(int(char))
+		if len(line) == 0 {
+			break
 		}
+
+		lineSet := bit.New()
+		for _, char := range line {
+			lineSet.Add(int(char))
+		}
+		fmt.Print("\nlineset:\n")
+		fmt.Print(lineSet)
+		set = set.And(lineSet)
+		fmt.Print("\nset:\n")
+		fmt.Print(set)
 	}
+	fmt.Print("\n")
 	return set
 }
 
