9e58f8749e802198341c0833031218c5a28414ba
ratty <ratty@inuits.eu>
2020-12-17 11:23:53 +0100

    day9 pt2

--- day9/main.go
+++ day9/main.go
@@ -20,19 +20,26 @@ func readInput(filename string) string {
 	return string(dat)
 }
 
-func pt1() {
+func parseString() []int {
 	inputs := strings.Split(readInput("input"), "\n")
+	nums := make([]int, 0, 64)
+	for i := 0; i < len(inputs); i++ {
+		next, _ := strconv.ParseInt(inputs[i], 10, 64)
+		nums = append(nums, int(next))
+	}
+	return nums
+}
+
+func pt1(inputs []int) int {
 	nums := [25]int{}
 	// initialise first 25 numbers
 	for i := 0; i < 25; i++ {
-		next, _ := strconv.ParseInt(inputs[i], 10, 64)
-		nums[i] = int(next)
+		nums[i] = inputs[i]
 	}
 
 	first := 0
 	for i := 25; i < len(inputs); i++ {
-		parsed, _ := strconv.ParseInt(inputs[i], 10, 64)
-		next := int(parsed)
+		next := inputs[i]
 		// find valid sum
 		isValid := false
 		for _, x := range nums {
@@ -54,11 +61,42 @@ func pt1() {
 			}
 		} else {
 			fmt.Printf("%v is not valid\n", next)
+			return next
+		}
+	}
+	panic("i'm expecting to find a number")
+}
+
+func pt2(inputs []int, target int) {
+	for i := 0; i < len(inputs); i++ {
+		acc := 0
+		j := i
+		for acc < target {
+			acc += inputs[j]
+			j++
+		}
+		if acc == target {
+			fmt.Printf("target %v found (%v), summing\n", target, acc)
+			min := target
+			max := 0
+			for ; i <= j; i++ {
+				fmt.Printf("%v ", inputs[i])
+				if inputs[i] < min {
+					min = inputs[i]
+				}
+				if inputs[i] > max {
+					max = inputs[i]
+				}
+
+			}
+			fmt.Printf("min: %v, max: %v, add: %v", min, max, min+max)
 			return
 		}
 	}
 }
 
 func main() {
-	pt1()
+	input := parseString()
+	target := pt1(input)
+	pt2(input, target)
 }
